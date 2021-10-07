417cdf53a800e1a4a1f157a1c93666c8adf18a76
TejaswiniBetina <68936901+tejaswini14345@users.noreply.github.com>
2021-10-07 19:14:55 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,6 +1,6 @@
 package hello;
 
-public class Greeter {
+//public class Greeter {
   public String sayHello() {
     return "Hello world- This is Venkata Naresh Mandhala your Trainer !! Hi or Bye is the string needed here.Testing Pipeline";
   }
